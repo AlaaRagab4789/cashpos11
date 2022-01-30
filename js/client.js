5609441d220dfd58419306f6961914f547fc446a
AlaaRagab4789 <AlaaRagab4789@gmail.com>
2022-01-30 09:51:38 +0200

    first commit

---
+++ js/client.js
@@ -0,0 +1,25 @@
+var client2 = document.getElementById("client2");
+var client3 = document.getElementById("client3");
+var client4 = document.getElementById("client4");
+
+
+client2.onclick = function()
+{
+
+location.replace("addclient.html");
+}
+
+
+client3.onclick = function()
+{
+
+location.replace("clientdata.html");
+}
+
+
+client4.onclick = function()
+{
+
+location.replace("clientdata.html");
+}
+
