aa63dfac334376f20b4c0e9d6c3f952f52cb3285
AlaaRagab4789 <95539812+AlaaRagab4789@users.noreply.github.com>
2022-02-01 13:42:47 +0200

    Add files via upload

---
+++ js/client-show.js
@@ -0,0 +1,19 @@
+
+
+var show = document.getElementById("show");
+var edit = document.getElementById("edit");
+
+
+show.onclick = function()
+{
+
+location.assign("client-show.html");
+}
+
+
+edit.onclick = function()
+{
+
+location.assign("addclient.html");
+}
+
